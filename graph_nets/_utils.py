d3d36c205316d969fb96f7e6113bd8f483f502a6
Michał Stolarczyk <michal.stolarczyk@nethone.com>
2020-05-29 17:04:36 +0200

    Pass is_training if model allows passing keyword arguments

--- graph_nets/_utils.py
+++ graph_nets/_utils.py
@@ -1,13 +1,9 @@
 import inspect
 
 
-def _has_argument(callable_, argument_name):
-  return argument_name in inspect.getfullargspec(callable_).args
-
-
 def _call_with_is_training_flag_if_possible(model, inputs, is_training):
-  if _has_argument(model, 'is_training'):
-    assert is_training is not None
+  argspec = inspect.getfullargspec(model)
+  if 'is_training' in argspec.args or argspec.varkw is not None:
     outputs = model(inputs, is_training=is_training)
   else:
     outputs = model(inputs)
